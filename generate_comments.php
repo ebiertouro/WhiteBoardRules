8f452e2777d8356bf7b7ff9e6d3c04e1c17eb872
ShaynaIs <46361951+ShaynaIs@users.noreply.github.com>
2024-02-20 18:09:17 -0500

    Removwe open tags for generate_comments.php

--- generate_comments.php
+++ generate_comments.php
@@ -1,9 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Generate Comments</title>
 <?php include "header.php"; ?>
     <h1>Generate Report Card Comments</h1>
 
