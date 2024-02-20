024ee41a34fc35d934c6f478e678aea4f7530212
ShaynaIs <46361951+ShaynaIs@users.noreply.github.com>
2024-02-20 18:40:09 -0500

    Menu Upload

--- header.php
+++ header.php
@@ -3,15 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Lato: 100,300,400,700|Luckiest+Guy|Oxygen:300,400"
+        rel="stylesheet">
     <link rel="stylesheet" href="styles.css" type="text/css" />
     <title><?php echo $title; ?></title>
 </head>
 <body>
     <div id="header">
-        <div class="logo-container">
-            <img src="LogoImage.png" width="80" id="logo" />
-            <img src="LogoSlogan.png" id="slogan" />
-        </div>
-    </div>
+        <ul class="navigation">
+            <li class="navItem">        
+                <div class="logo-container">
+                    <img src="LogoImage.png" width="80" id="logo" />
+                    <img src="LogoSlogan.png" id="slogan" />
+                </div>
+            </li>
+            <li class="navItem"><a class="navA activeA" href="./main.html">Classes</a></li>
+            <li class="navItem"><a class="navA" href="./students.html">Students</a></li>
+            <li class="navItem"><a class="navA" href="./assignments.html">Assignments</a></li>
+            <li class="navItem"><a class="navA" href="./table.html">Generate Report Cards</a></li>
+        </ul>
+    </div>  
     <hr />
 
