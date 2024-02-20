4b3a8d9c29fc02bfb09b0e5e09dfb02135eddc77
ShaynaIs <46361951+ShaynaIs@users.noreply.github.com>
2024-02-20 18:10:37 -0500

    Added title header header.php

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css" type="text/css" />
+    <title><?php echo $title; ?></title>
 </head>
 <body>
     <div id="header">
