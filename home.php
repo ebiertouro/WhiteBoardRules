c0ff849b82938d7f2d96c28d0e1c7bf1b3c865b2
ShaynaIs <46361951+ShaynaIs@users.noreply.github.com>
2024-02-20 17:54:27 -0500

    Remove open tags in home.php

--- home.php
+++ home.php
@@ -1,10 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Home</title>
     <?php include "header.php"; ?>
-</head>
-<body>
     <h1>Welcome to WhiteBoardRules</h1>
     <p>Streamline the process of report card making with our platform.</p>
     <a href="login.php">Login</a>
