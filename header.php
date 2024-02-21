3ddd9f757a166cad539179d87f0c51c7e35a83aa
ShaynaIs <46361951+ShaynaIs@users.noreply.github.com>
2024-02-20 19:35:32 -0500

    Seperated menu from header

--- header.php
+++ header.php
@@ -11,19 +11,8 @@
     <title><?php echo $title; ?></title>
 </head>
 <body>
-    <div id="header">
-        <ul class="navigation">
-            <li class="navItem">        
-                <div class="logo-container">
-                    <img src="LogoImage.png" width="80" id="logo" />
-                    <img src="LogoSlogan.png" id="slogan" />
-                </div>
-            </li>
-            <li class="navItem"><a class="navA activeA" href="./main.html">Classes</a></li>
-            <li class="navItem"><a class="navA" href="./students.html">Students</a></li>
-            <li class="navItem"><a class="navA" href="./assignments.html">Assignments</a></li>
-            <li class="navItem"><a class="navA" href="./table.html">Generate Report Cards</a></li>
-        </ul>
-    </div>  
+    <?php 
+        include "index.php"; 
+    ?>
     <hr />
 
--- index.php
+++ index.php
@@ -1,7 +1,14 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-
+<div id="header">
+    <ul class="navigation">
+        <li class="navItem">        
+            <div class="logo-container">
+                <img src="LogoImage.png" width="80" id="logo" />
+                <img src="LogoSlogan.png" id="slogan" />
+            </div>
+        </li>
+        <li class="navItem"><a class="navA activeA" href="./main.html">Classes</a></li>
+        <li class="navItem"><a class="navA" href="./students.html">Students</a></li>
+        <li class="navItem"><a class="navA" href="./assignments.html">Assignments</a></li>
+        <li class="navItem"><a class="navA" href="./table.html">Generate Report Cards</a></li>
+    </ul>
+</div>  
