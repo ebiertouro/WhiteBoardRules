1ab5f266c89e7ad3f6fa746dbdce60ef27956e2f
ShaynaIs <46361951+ShaynaIs@users.noreply.github.com>
2024-02-20 19:47:30 -0500

    Added active variable to menu

--- menu.php
+++ menu.php
@@ -6,9 +6,9 @@
                 <img src="LogoSlogan.png" id="slogan" />
             </div>
         </li>
-        <li class="navItem"><a class="navA activeA" href="./main.html">Classes</a></li>
-        <li class="navItem"><a class="navA" href="./students.html">Students</a></li>
-        <li class="navItem"><a class="navA" href="./assignments.html">Assignments</a></li>
-        <li class="navItem"><a class="navA" href="./table.html">Generate Report Cards</a></li>
+        <li class="navItem"><a class="nav <?php if ( $title == "Home" ) { echo "active"; } ?>" href="./main.html">Classes</a></li>
+        <li class="navItem"><a class="nav <?php if ( $title == "Students" ) { echo "active"; } ?>" href="./students.html">Students</a></li>
+        <li class="navItem"><a class="nav <?php if ( $title == "Assignments" ) { echo "active"; } ?>" href="./assignments.html">Assignments</a></li>
+        <li class="navItem"><a class="nav <?php if ( $title == "Table" ) { echo "active"; } ?>" href="./table.html">Generate Report Cards</a></li>
     </ul>
 </div>  
