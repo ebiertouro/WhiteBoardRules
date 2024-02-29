c53ef2084b4de19601ea25c3cf5fc99f3abafdfe
ShaynaIs <46361951+ShaynaIs@users.noreply.github.com>
2024-02-29 17:22:00 -0500

    Updated login process to check against DB

--- process_login.php
+++ process_login.php
@@ -1,31 +1,53 @@
-   <?php 
+    <?php 
         $title = "Sign-Up Response";
         include "header.php"; 
-    session_start();
-    
-    $UserNameArray = ['e.shapiro', 's.israel', 'professor'];
-    $PasswordArray = ['xxx', '1234', 'iloveteaching'];
-   
-   
-    $postedUsername = isset($_POST['username']) ? $_POST['username'] : '';
-    $postedPassword = isset($_POST['password']) ? $_POST['password'] : '';
+        session_start();
+    ?>
+    <?php
+        // get users input
+        $postedUsername = isset($_POST['username']) ? $_POST['username'] : '';
+        $postedPassword = isset($_POST['password']) ? $_POST['password'] : '';
 
-// Check if the posted username is in the array
-    if (in_array($postedUsername, $UserNameArray) && in_array($postedPassword, $PasswordArray)) {
-              setcookie('UserName', $postedUsername);
-              //set the session status to logged in
-          $_SESSION["LoggedIn"] = TRUE;
-          //direct the user to the content page
-          
-    }
-    else {
-        
-        $_SESSION["LoggedIn"] = FALSE;
-            // Set the number of seconds to wait before redirecting
-    
-    }
-    $ContentPage = 'content.php';
-    echo "<a href='{$ContentPage}'> Click here to view your students, record grades, and generate report cards</a>.";
+        // connect to DB
+        $dbhost = '127.0.0.1';
+        $dbuser = 'root';
+        $dbpass = '';
+        $dbname = 'white_board_rules';
+        $mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+         
+        // check connection
+        if($mysqli->connect_errno ) {
+            printf("Connect failed: %s<br />", $mysqli→connect_error);
+            exit();
+        }
+
+        // check if server is alive
+        if (!($mysqli->ping())) {
+            printf ("Error: %s\n", $mysqli->error);
+            exit();
+        }
+
+        printf('Connected successfully.<br />');
+         
+        // Run Query
+        $sql = "SELECT `username`, `password` FROM `authorizedusers` WHERE `username` = '" . $postedUsername . "' AND `password`= '" . $postedPassword . "'";
+        $result = $mysqli->query($sql);
+           
+        if ($result->num_rows == 1) {
+            setcookie('UserName', $postedUsername);
+            $_SESSION["LoggedIn"] = TRUE;
+            printf('Logged in.<br />');
+        } else {
+            $_SESSION["LoggedIn"] = FALSE;
+            printf('Not logged in.<br />');
+        }
+
+        // Close DB
+        mysqli_free_result($result);
+        $mysqli->close();
+
+        $ContentPage = 'content.php';
+        echo "<a href='{$ContentPage}'> Click here to view your students, record grades, and generate report cards</a>.";
 
     include "footer.php"; ?>
 
