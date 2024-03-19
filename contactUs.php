bb8b61918e4c2fc62713c44472eaf15abe2361ee
ShaynaIs <46361951+ShaynaIs@users.noreply.github.com>
2024-03-19 14:27:34 -0400

    Add contact page

---
+++ contactUs.php
@@ -0,0 +1,90 @@
+<?php 
+        $title = "ContactUs";
+        include "header.php"; 
+    ?>
+
+
+<?php
+require "../../directory/PHPMailer-master/PHPMailerAutoload.php";
+$errors = [];
+$errorMessage = '';
+
+if (!empty($_POST)) {
+   $name = $_POST['name'];
+   $email = $_POST['email'];
+   $message = $_POST['message'];
+
+   if (empty($name)) {
+       $errors[] = 'Name is empty';
+   }
+
+   if (empty($email)) {
+       $errors[] = 'Email is empty';
+   } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+       $errors[] = 'Email is invalid';
+
+   }
+
+
+   if (empty($message)) {
+       $errors[] = 'Message is empty';
+   }
+
+   if (!empty($errors)) {
+       $allErrors = join('<br/>', $errors);
+       $errorMessage = "<p style='color: red;'>{$allErrors}</p>";
+   } else {
+       $mail = new PHPMailer();
+       $mail->isSMTP();
+       $mail->SMTPSecure = 'ssl';
+       $mail->SMTPAuth = true;
+       $mail->SMTPDebug=0; //4; //this is very verbose, just for testing, change to 0
+       $mail->Host = 'smtp.gmail.com';
+       $mail->Port = 465;
+       $mail->Username = 'white.board.rules.24@gmail.com';
+       $mail->Password = 'unql psmc dygz xjbk';
+       $mail->setFrom('white.board.rules.24@gmail.com');
+       $mail->addAddress('white.board.rules.24@gmail.com');
+       $mail->Subject = 'New message from your website';
+
+       // Enable HTML if needed
+       $mail->isHTML(true);
+       $bodyParagraphs = ["Name: {$name}", "Email: {$email}", "Message:", nl2br($message)];
+       $body = join('<br />', $bodyParagraphs);
+       $mail->Body = $body;
+       echo $body;
+
+       if($mail->send()){
+           header('Location: thankYou.php'); // Redirect to 'thank you' page. Make sure you have it
+       } else {
+           $errorMessage = 'Oops, something went wrong. Mailer Error: ' . $mail->ErrorInfo;
+       }
+
+   }
+
+}
+
+?>
+
+ <form action="" method="post" id="contact-form">
+   <h2>Contact us</h2>
+   <p>
+     <label>First Name:</label>
+     <input name="name" type="text"/>
+   </p>
+   <p>
+     <label>Email Address:</label>
+     <input style="cursor: pointer;" name="email" type="text"/>
+   </p>
+   <p>
+     <label>Message:</label>
+     <textarea name="message"></textarea>
+   </p>
+   <p>
+     <input type="submit" value="Send"/>
+   </p>
+ </form>
+
+
+
+<?php include "footer.php"; ?>
---
+++ thankYou.php
@@ -0,0 +1,9 @@
+<?php 
+    $title = "Home";
+    include "header.php"; 
+?>
+<p> Thank you for your submission! </br> We will try to get back to you in a timeely manner!</p>
+    
+<?php include "footer.php"; ?>
+
+
