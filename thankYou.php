3e0716f861af684139de4b64dbe95d3ba36910e5
ShaynaIs <46361951+ShaynaIs@users.noreply.github.com>
2024-03-19 15:12:25 -0400

    Edit typo

--- thankYou.php
+++ thankYou.php
@@ -2,7 +2,7 @@
     $title = "Home";
     include "header.php"; 
 ?>
-<p> Thank you for your submission! </br> We will try to get back to you in a timeely manner!</p>
+<p> Thank you for your submission! </br> We will try to get back to you in a timely manner!</p>
     
 <?php include "footer.php"; ?>
 
