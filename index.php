939574505ccb0970e35e984a47516727d1201817
ShaynaIs <46361951+ShaynaIs@users.noreply.github.com>
2024-03-19 14:26:40 -0400

    Edit index.php
    Add sign up link

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <h1>Welcome to WhiteBoardRules</h1>
     <p>Streamline the process of report card making with our platform.</p>
     <a href="login.php">Login</a>
+    <a href="signUp.php">Sign Up</a>
     
     <?php include "footer.php"; ?>
 
