d5ff4591d2a0854824e75bfc93bce5a24e3a0ae2
ShaynaIs <46361951+ShaynaIs@users.noreply.github.com>
2024-03-19 22:21:05 -0400

    Update menu.php

--- menu.php
+++ menu.php
@@ -8,7 +8,6 @@
                 <img src="LogoSlogan.png" id="slogan" />
             </div>
         </li>
-        <li class="navItem"><a class="nav <?php if ( $title == "Home" ) { echo "active"; } ?>" href="./index.php">Home</a></li>
         <li class="navItem"><a class="nav <?php if ( $title == "Students" ) { echo "active"; } ?>" href="./students.php">Students</a></li>
         <li class="navItem"><a class="nav <?php if ( $title == "Assignments" ) { echo "active"; } ?>" href="./assignments.php">Assignments</a></li>
         <li class="navItem"><a class="nav <?php if ( $title == "Report Cards" ) { echo "active"; } ?>" href="./table.php">Report Cards</a></li>
