67eed6c3e22403e8c0d5b58e0913e78926b70c27
ebiertouro <121737634+ebiertouro@users.noreply.github.com>
2024-06-26 14:51:31 -0400

    Update grade_saved.php

--- updatedphp/grade_saved.php
+++ updatedphp/grade_saved.php
@@ -5,6 +5,9 @@ include "header.php";
 
 echo "<p>Your grade has been saved!</p>";
 
+
+echo "<a class='btn' href='assignments.php'>Save Another Grade</a>";
+
 //get the student's name from the database!
 
 include "footer.php";
