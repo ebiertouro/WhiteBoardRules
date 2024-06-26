72bc0a4b5efc1e8dbc612c0346cb6b04a3805f23
ebiertouro <121737634+ebiertouro@users.noreply.github.com>
2024-06-26 16:34:31 -0400

    Delete php directory

--- php/confirmation.php
+++
@@ -1,9 +0,0 @@
- <?php include "header.php"; ?>
-
-   
-    <h1>Your grade has been saved.</h1>
-    <p>You can view, edit, remove this grade by clicking <>this link<> on your portal.</p>
-    
-        
-<?php include "footer.php"; ?>
-
--- php/thank_you.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    $title = "Home";
-    include "header.php"; 
-?>
-<p> Thank you for your submission! </br> We will try to get back to you in a timely manner!</p>
-    
-<?php include "footer.php"; ?>
-
-
